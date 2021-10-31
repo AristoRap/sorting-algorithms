6901b0ef5bb782f6a61f56fd248489654831f4ee
Aristotelis Rapai <aristotelisrapai@gmail.com>
2021-10-31 22:45:03 +0100

    Defined SortingAlgorithms class and refactored code in helpers

--- algorithms/sorting_algorithms.rb
+++ algorithms/sorting_algorithms.rb
@@ -1,1 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'bubble_sort'
+
+# Class wrapper for various implemented sorting algirithms.
+class SortingAlgorithm
+  def self.bubble_sort(array, ord = 'desc')
+    case ord
+    when 'asc'
+      bubble_sort_desc(array).reverse
+    else
+      bubble_sort_desc(array)
+    end
+  end
+end
--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def timer
   start_time = Time.now
   yield
@@ -13,21 +15,22 @@ end
 
 def valid_option?(option)
   options = [1, 2]
-  return options.include?(option.to_i)
+  options.include?(option.to_i)
 end
 
 def valid_order?(order)
-  orders = ['asc', 'desc']
-  return orders.include?(order)
+  orders = %('asc', 'desc')
+  orders.include?(order)
 end
 
 def choose_option(option)
-  if option == 1
+  case option
+  when 1
     puts 'Enter a max number to define the range (max. 10000):'
     puts "1\n.\n.\n."
     number = gets.chomp.to_i
     array = (1..number).to_a
-  elsif option == 2
+  when 2
     puts 'Enter a max number of random numbers to add (max. 10000):'
     number = gets.chomp.to_i
     array = []
@@ -35,14 +38,14 @@ def choose_option(option)
   else
     puts "That's not a choice - Is it?\nTry again (or type 'exit' to cancel):"
   end
-  return array
+  array
 end
 
 def choose_order(order)
   case order
   when 'asc'
-    return 'ascending'
+    'ascending'
   when 'desc'
-    return 'descending'
+    'descending'
   end
 end
