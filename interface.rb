d99054118356e449f26fb125fce87b1c5564ce64
Aristotelis Rapai <aristotelisrapai@gmail.com>
2021-10-31 16:51:15 +0100

    Defined methods to handle option and order inputs and to exit program - implemented in interface

--- interface.rb
+++ interface.rb
@@ -11,49 +11,22 @@ puts '---------------------'
 puts "Option 2: Range of random numbers - Type '2'"
 puts 'Enter the max number of random numbers you would like to provide
 as input for the sort'
+puts '---------------------'
+puts "Exit: Type 'exit' to exit the program"
 option = nil
+value = choose_option(option)
+return exit_interface if value == 'exit'
 
-until option == 1 || option == 2 || option == 'exit'
-  option = gets.chomp.to_i
-  return '-Exit Successful-' if option == 'exit'
-
-  if option == 1
-    puts 'Enter a max number to define the range (max. 10000):'
-    puts "1\n.\n.\n."
-    number = gets.chomp.to_i
-    array = (1..number).to_a
-  elsif option == 2
-    puts 'Enter a max number of random numbers to add (max. 10000):'
-    number = gets.chomp.to_i
-    array = []
-    number.times { array << rand(100_00) }
-  else
-    puts "That's not a choice - Is it?\n Try again (or type 'exit' to cancel):"
-  end
-end
-
-value = array.shuffle
+value = value.shuffle
 puts 'Here is the random list of numbers generated:'
 p value
 
-puts 'Oh, by the way - Do you want to sort in ascending or descending order?'
+puts 'How do you want to sort? In ascending or descending order?'
 puts "Type 'asc' or 'desc'"
 
 order = nil
-until order == 'asc' || order == 'desc' || order == 'exit'
-  order = gets.chomp
-  return '-Exit Successful-' if order == 'exit'
-
-  case order
-  when 'asc'
-    order_description = 'Ascending'
-  when 'desc'
-    order_description = 'Descending'
-  else
-    "Hmmm, I'm pretty sure that's not an option - Try again.
-    (Oh, btw, you can type 'exit' to cancel)"
-  end
-end
+order_description = choose_order(order)
+return exit_interface if order_description == 'exit'
 
 puts '---------------------'
 puts "aaaand here is the list of numbers sorted in #{order_description.downcase} order:"
