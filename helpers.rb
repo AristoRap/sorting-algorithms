f281dff31be7a43c5354cd6e62a56f26a464694f
Aristotelis Rapai <aristotelisrapai@gmail.com>
2021-10-31 18:29:10 +0100

    Added 2 input validations in helpers and implemented in interface. Created placeholder for helpers spec

--- helpers.rb
+++ helpers.rb
@@ -1,44 +1,39 @@
-def choose_option(option)
-  until option == 1 || option == 2 || option == 'exit'
-    option = gets.chomp
-    return option if option == 'exit'
-    option = option.to_i
+def exit_interface
+  puts ''
+  puts '---Exit Successful---'
+end
 
-    if option == 1
-      puts 'Enter a max number to define the range (max. 10000):'
-      puts "1\n.\n.\n."
-      number = gets.chomp.to_i
-      array = (1..number).to_a
-    elsif option == 2
-      puts 'Enter a max number of random numbers to add (max. 10000):'
-      number = gets.chomp.to_i
-      array = []
-      number.times { array << rand(100_00) }
-    else
-      puts "That's not a choice - Is it?\n Try again (or type 'exit' to cancel):"
-    end
+def valid_option?(option)
+  options = [1, 2]
+  return options.include?(option.to_i)
+end
+
+def valid_order?(order)
+  orders = ['asc', 'desc']
+  return orders.include?(order)
+end
+
+def choose_option(option)
+  if option == 1
+    puts 'Enter a max number to define the range (max. 10000):'
+    puts "1\n.\n.\n."
+    number = gets.chomp.to_i
+    array = (1..number).to_a
+  elsif option == 2
+    puts 'Enter a max number of random numbers to add (max. 10000):'
+    number = gets.chomp.to_i
+    array = []
+    number.times { array << rand(100_00) }
   end
   return array
 end
 
 def choose_order(order)
-  until order == 'asc' || order == 'desc' || order == 'exit'
-    order = gets.chomp
-    return order if order == 'exit'
-
-    case order
-    when 'asc'
-      order_description = 'Ascending'
-    when 'desc'
-      order_description = 'Descending'
-    else
-      puts "Hmmm, I'm pretty sure that's not an option - Try again.\n(Oh, btw, you can type 'exit' to cancel)"
-    end
+  case order
+  when 'asc'
+    order_description = 'Ascending'
+  when 'desc'
+    order_description = 'Descending'
   end
   return order_description
-end
-
-def exit_interface
-  puts ''
-  puts '---Exit Successful---'
 end
--- interface.rb
+++ interface.rb
@@ -1,5 +1,6 @@
 require_relative 'algorithms/all_algorithms'
 require_relative 'helpers'
+require 'pry-byebug'
 
 puts '-----Bubble Sort-----'
 puts "Option 1: Range of random consecutive numbers - Type '1'"
@@ -13,21 +14,32 @@ puts 'Enter the max number of random numbers you would like to provide
 as input for the sort'
 puts '---------------------'
 puts "Exit: Type 'exit' to exit the program"
-option = nil
-value = choose_option(option)
-return exit_interface if value == 'exit'
+option = gets.chomp
 
+until valid_option?(option) || option == 'exit'
+  puts "That's not a choice - Is it?\nTry again (or type 'exit' to cancel):"
+  option = gets.chomp
+end
+
+return exit_interface if option == 'exit'
+
+value = choose_option(option.to_i)
 value = value.shuffle
 puts 'Here is the random list of numbers generated:'
 p value
 
 puts 'How do you want to sort? In ascending or descending order?'
 puts "Type 'asc' or 'desc'"
+order = gets.chomp
 
-order = nil
+until valid_order?(order) || order == 'exit'
+  puts "Hmmm, I'm pretty sure that's not an option - Try again.\n(Oh, btw, you can type 'exit' to cancel)"
+  order = gets.chomp
+end
+
+return exit_interface if order == 'exit'
+
 order_description = choose_order(order)
-return exit_interface if order_description == 'exit'
-
 puts '---------------------'
 puts "aaaand here is the list of numbers sorted in #{order_description.downcase} order:"
 sorted_arr = bubble_sort_desc(value)
